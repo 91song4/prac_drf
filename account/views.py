7dff9a807ef476c32d685452c7da266150549af6
91song4 <91song4@daum.net>
2023-10-05 14:03:46 +0900

    feat: 기존 django 방식 view 구현

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+# 기존 장고 방식
+def index(request):
+    return HttpResponse('hello world!!')
+
+
+# DRF 방식
