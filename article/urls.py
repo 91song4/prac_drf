50f2f077c0e6515f135c5bc3c2ee1e7a20133f5e
91song4 <91song4@daum.net>
2023-10-09 14:49:29 +0900

    read_articles url 연결

--- article/urls.py
+++ article/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from article import views
 
 urlpatterns = [
-    path('create/', views.create_article)
+    path('create/', views.create_article),
+    path('read/', views.read_articles),
 ]
