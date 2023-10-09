bbf73c92acb48b9475c18657395927862f2b4965
91song4 <91song4@daum.net>
2023-10-09 13:47:04 +0900

    초기화

--- board/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Board(models.Model):
-    title = models.CharField(max_length=200)
-    create_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
