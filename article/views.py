df0a5c2903b8170470c59c28ddb426b7789c0d71
91song4 <91song4@daum.net>
2023-10-09 14:49:02 +0900

    feat: 모든 aritle 가져오기 view 작성

--- article/views.py
+++ article/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from article.models import Article
 
 from article.serializers import ArticleSerializer
 # Create your views here.
@@ -13,3 +14,11 @@ def create_article(req):
         return Response(serializer.data, status=201)
 
     return Response(serializer.errors, status=400)
+
+
+@api_view(['get'])
+def read_articles(req):
+    articles = Article.objects.all().order_by('-create_date')
+    serializer = ArticleSerializer(articles, many=True)
+
+    return Response(serializer.data, status=200)
