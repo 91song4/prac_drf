00a8c909e02558ff01e799692680214a5a8b1628
91song4 <91song4@daum.net>
2023-10-09 14:35:11 +0900

    fix: CharField메소드에 인수를 아무것도 적지 않으면 255인 줄 알았지만 명시적으로 적어줘야함

--- article/models.py
+++ article/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Article(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=255)
     content = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
