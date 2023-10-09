02194db4e289a0c644b10f48cfcca4c28e1fbcd0
91song4 <91song4@daum.net>
2023-10-09 14:21:52 +0900

    feat: article 생성 view 작성

--- article/views.py
+++ article/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from article.serializers import ArticleSerializer
 # Create your views here.
+
+
+@api_view(['post'])
+def create_article(req):
+    serializer = ArticleSerializer(req.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+
+    return Response(serializer.errors, status=400)
