bdfb13520ea5a68714fcc02fb83b5615e5322e40
91song4 <91song4@daum.net>
2023-10-09 15:04:10 +0900

    chore: update_article url 연결

--- article/urls.py
+++ article/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('create/', views.create_article),
     path('read/', views.read_articles),
     path('read/<int:article_id>/', views.read_article),
+    path('update/<int:article_id>/', views.update_article),
 ]
